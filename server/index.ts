a490d2753f45ae20b3d826c285dc654a73e5c12a
Arciiix <artur.nowak100@gmail.com>
2022-06-04 18:52:15 +0200

    Init socket on server

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,23 @@
 import express from "express";
+import { Server, Socket } from "socket.io";
 import config from "./config";
 import logger from "./logger";
 
 const app = express();
 
-app.listen(config.PORT, () => {
+const server = app.listen(config.PORT, () => {
   logger.info(`App has started on port ${config.PORT}`);
+});
+const io = new Server(server, {
+  cors: {
+    origin: ["https://admin.socket.io"],
+  },
+});
+
+io.on("connection", (socket: Socket) => {
+  logger.info(`New socket with id ${socket.id} has connected!`);
+
+  socket.on("disconnect", (reason) => {
+    logger.info(`Socket ${socket.id} has disconnected - ${reason}`);
+  });
 });
